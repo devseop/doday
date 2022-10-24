a91d8ddb832102d605402c85224d2f3584a5350f
devseop <cocc263@gmail.com>
2022-10-24 17:45:52 +0900

    :art: add setting icon area

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -5,9 +5,12 @@ import { day, today } from '../lib/date';
 const MainPage = () => {
   return (
     <div className="flex-column items-center">
-      <div className="mb-5">
-        <p className="text-xxl font-bold leading-[34px]">Today is {day}day</p>
-        <p className="text-xxl font-bold leading-[34px]">{today}</p>
+      <div className="flex justify-between items-center">
+        <div className="mb-5">
+          <p className="text-xxl font-bold leading-[34px]">Today is {day}day</p>
+          <p className="text-xxl font-bold leading-[34px]">{today}</p>
+        </div>
+        <div>setting img</div>
       </div>
       <Card />
     </div>
