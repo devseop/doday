233917dd83f4dee6283310a59760ed491b92b3bb
devseop <cocc263@gmail.com>
2022-10-25 14:50:05 +0900

    :heavy_plus_sign: add Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import MainPage from 'pages/MainPage';
 import React from 'react';
+import Router from './router';
 import './App.css';
 
 function App() {
   return (
     <div className="box-border bg-bg h-screen pt-6 px-5">
-      <MainPage />
+      <Router />
     </div>
   );
 }
